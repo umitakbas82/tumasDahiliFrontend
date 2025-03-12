b95f2e8cd28feedc198c6390c8841415dffd2caa
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-12 10:01:50 +0300

    material added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component,inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [ButtonModule,TableModule,MatIconModule,MatButtonModule,MatToolbarModule ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
+  readonly dialog = inject(MatDialog);
+
+  openDialog(){
+    
+  }
+
 
 }
